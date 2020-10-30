916cd77f9d3722cf81dc36ef4dfe081db2d00fa6
Olga Emelyanova <volha.emelianova@gmail.com>
2020-10-30 21:59:25 +0300

    Change to GET method

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from bookapi import bookapi
 
 import os, signal
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s,f: os._exit(0))
 
-@app.route("/")
+@app.route("/books/book", methods=['GET'])
 def get_book():
-    page = '<html><body><h1>'
-    page += bookapi.get_book()
-    page += '</h1></body></html>'
-    return page
+    book = bookapi.get_book()
+    return jsonify(book)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
