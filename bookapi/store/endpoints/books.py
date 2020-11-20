aa5f49fd5b5fcd68a15f12cc543897fae054534c
Olga Emelyanova <volha.emelianova@gmail.com>
2020-11-20 19:18:34 +0300

    Add book cover (image) method

--- bookapi/store/endpoints/books.py
+++ bookapi/store/endpoints/books.py
@@ -1,5 +1,8 @@
-from flask import request
+from flask import request, send_file
 from flask_restplus import Resource
+from PIL import Image
+import io
+
 from bookapi.store.serializers import book
 from bookapi.restplus import api
 from database.models import Book
@@ -23,3 +26,19 @@ class BookItem(Resource):
     @api.marshal_with(book)
     def get(self, id):
         return Book.query.filter(Book.id == id).one()
+
+
+@ns.route('/cover/<int:id>')
+@api.response(404, 'Cover image not found')
+class BookCover(Resource):
+	
+	@api.representation('image/png')
+	def get(self, id):
+		file = io.BytesIO()
+		img = Image.new('RGBA', (50, 50), (70, 0, 0, 255))
+		img.save(file, 'png')
+		file.seek(0)
+		return send_file(file,
+	                     as_attachment=True,
+	                     attachment_filename='annotated.png',
+	                     mimetype='image/png')
